05bfe50f5442bde2e7655fef1aad014e8e4950e0
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-05-29 10:54:46 +0300

    Add media files

---
+++ client/src/assets/images/gallery/hackathon/index.ts
@@ -0,0 +1,5 @@
+import yr2024 from "./yr2024";
+
+const hackathon = { yr2024 };
+
+export default hackathon;
--- client/src/assets/images/gallery/index.ts
+++ client/src/assets/images/gallery/index.ts
@@ -2,16 +2,18 @@ import airfield from "./airfield";
 import conference from "./conference";
 import expo from "./expo";
 import graduation from "./graduation";
+import hackathon from "./hackathon";
 import innovation from "./innovation-forum";
 import projects from "./projects";
 
 const gallery = {
-	airfield: airfield,
-	conference: conference,
-	expo: expo,
-	graduation: graduation,
-	innovation: innovation,
-	projects: projects,
+	airfield,
+	conference,
+	expo,
+	graduation,
+	hackathon,
+	innovation,
+	projects,
 };
 
 export default gallery;
