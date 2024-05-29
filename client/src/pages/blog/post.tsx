cfd81b739a896053918cf0de74c2f8d35c5951c4
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-05-29 10:57:46 +0300

    Update files

--- client/src/pages/blog/post.tsx
+++ client/src/pages/blog/post.tsx
@@ -26,9 +26,7 @@ import Partial from "@src/partials";
 export default function Post() {
 	const { post } = useParams();
 
-	const blog = data.blogs.find(
-		(item) => utility.parser.linkify(item.title) == post
-	);
+	const blog = data.blogs.find(item => utility.parser.linkify(item.title) == post);
 
 	return (
 		<Layout.Page
@@ -46,37 +44,24 @@ export default function Post() {
 				{blog && (
 					<Stack gap={"md"}>
 						<AspectRatio ratio={1920 / 1080}>
-							<Image
-								src={blog.image}
-								alt={blog.title}
-								radius={"xs"}
-							/>
+							<Image src={blog.image} alt={blog.title} radius={"xs"} />
 						</AspectRatio>
 						<Container size={"sm"}>
 							<Stack gap={"md"}>
 								<Stack gap={"xs"}>
 									<Title order={2}>{blog.title}</Title>
-									<Group
-										gap={"xs"}
-										fz={"xs"}
-										fw={500}
-										c={"pri.6"}
-									>
-										<Text
-											component="p"
-											fz={"inherit"}
-											fw={"inherit"}
-										>
+									<Group gap={"xs"} fz={"xs"} fw={500} c={"pri.6"}>
+										<Text component="p" fz={"inherit"} fw={"inherit"}>
 											{blog.date}
 										</Text>{" "}
-										/
+										{/* /
 										<Text
 											component="p"
 											fz={"inherit"}
 											fw={"inherit"}
 										>
 											{blog.views} Views
-										</Text>
+										</Text> */}
 										{/* /
 												<Text
 													component="p"
@@ -91,16 +76,14 @@ export default function Post() {
 								<Stack gap={"lg"} fz={"sm"}>
 									<Text inherit>{blog.desc.p[0]}</Text>
 									{blog.desc.quotes && (
-										<Blockquote
-											cite={blog.desc.quotes[0].cite}
-										>
+										<Blockquote cite={blog.desc.quotes[0].cite}>
 											{blog.desc.quotes[0].title}
 										</Blockquote>
 									)}
 									<Text inherit>{blog.desc.p[1]}</Text>
 									{blog.desc.images && (
 										<Grid align="center">
-											{blog.desc.images.map((item) => (
+											{blog.desc.images.map(item => (
 												<GridCol
 													key={item.title}
 													span={{
@@ -108,30 +91,17 @@ export default function Post() {
 														sm: 4,
 													}}
 												>
-													<Image
-														src={item.image}
-														alt={item.title}
-														loading="lazy"
-													/>
+													<Image src={item.image} alt={item.title} loading="lazy" />
 												</GridCol>
 											))}
 										</Grid>
 									)}
 									<Text inherit>{blog.desc.p[2]}</Text>
 									{blog.desc.list && (
-										<List
-											spacing={"xs"}
-											size={"sm"}
-											listStyleType="none"
-										>
-											{blog.desc.list.map((item) => (
+										<List spacing={"xs"} size={"sm"} listStyleType="none">
+											{blog.desc.list.map(item => (
 												<ListItem key={item.title}>
-													<Text
-														component="span"
-														inherit
-														fw={500}
-														c={"pri.6"}
-													>
+													<Text component="span" inherit fw={500} c={"pri.6"}>
 														{item.title}
 													</Text>
 													: {item.desc}
@@ -141,7 +111,7 @@ export default function Post() {
 									)}
 									<Text inherit>{blog.desc.p[3]}</Text>
 									{blog.desc.p.map(
-										(item) =>
+										item =>
 											blog.desc.p.indexOf(item) > 3 && (
 												<Text key={item} inherit>
 													{item}
