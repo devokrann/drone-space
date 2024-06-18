342c698b9110299019abee1b9418f84fcfa7aa89
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-06-18 15:57:36 +0300

    Remove holiday popups

--- client/src/assets/images/popups/holidays/index.ts
+++
@@ -1,5 +0,0 @@
-import easter from "./easter.jpeg";
-
-const holiday = { easter };
-
-export default holiday;
--- client/src/assets/images/popups/index.ts
+++ client/src/assets/images/popups/index.ts
@@ -1,11 +1,9 @@
 import courses from "./courses";
 import intakes from "./intakes";
-import holiday from "./holidays";
 
 const popups = {
 	intakes,
 	courses,
-	holiday,
 };
 
 export default popups;
